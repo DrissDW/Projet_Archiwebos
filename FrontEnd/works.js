9ff5cfbc722f96f01b6aa930d84552ed53399a3a
DrissDW <driss.mallali@yahoo.com>
2023-04-15 11:16:48 +0200

    Added listener for the "objet" button

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -2,32 +2,43 @@
 const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 
-console.log(works);
+// console.log(works);
 
-//Fonction qui génére toute la gallery de la page web:
+//Fonction qui génére  la gallery de la page web:
 
-function generateProjects(){
+function generateProjects(works) {
+  for (let i = 0; i < works.length; i++) {
+    const project = works[i];
 
-    for (let i = 0; i < works.length; i++) {
-        const project = works[i];
-        
-        const titleElement = document.createElement("figcaption");
-        titleElement.innerText = project.title;
-        
-        const imageElement = document.createElement("img");
-        imageElement.src = project.imageUrl;
-        
-        const figureElement = document.createElement("figure");
-        
-        const galleryProjects = document.querySelector(".gallery");
-        
-        // Ajouter la figure à l'élément parent spécifique
-        galleryProjects.appendChild(figureElement);
-        
-        // Ajouter les éléments enfants à la figure
-        figureElement.appendChild(imageElement);
-        figureElement.appendChild(titleElement);
-    }
-};
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = project.title;
 
-generateProjects();
+    const imageElement = document.createElement("img");
+    imageElement.src = project.imageUrl;
+
+    const figureElement = document.createElement("figure");
+
+    const galleryProjects = document.querySelector(".gallery");
+
+    // Ajouter la figure à l'élément parent spécifique
+    galleryProjects.appendChild(figureElement);
+
+    // Ajouter les éléments enfants à la figure
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(titleElement);
+  }
+}
+
+generateProjects(works);
+
+//Ajout du Listener pour filtrer les projets qui on pour catégorie 1 (tous)
+const bouttonFilterObjet = document.querySelector(".btn-objets");
+bouttonFilterObjet.addEventListener("click", function () {
+  const filterObjet = works.filter(function (objet) {
+    return objet.category.name === "Objets";
+  });
+
+  // effacement de l'écran et régénération de la page
+  document.querySelector(".gallery").innerHTML = "";
+  generateProjects(filterObjet);
+});
